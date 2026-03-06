dc4954f5230c19a355f9aa151822fa928c788163
Wagner-Vale12 <wagner6528@gmail.com>
2026-03-05 22:18:12 -0300

    Ajuste no icone do check

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react"
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 import Button from "./Button"
 
@@ -16,7 +16,9 @@ function onSeeDetailsClick(task) {
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white font-bold p-2 rounded-md ${task.isCompleted && 'line-through'}`}>
+          <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white font-bold p-2 rounded-md
+           ${task.isCompleted && 'line-through'}`}>
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
 
